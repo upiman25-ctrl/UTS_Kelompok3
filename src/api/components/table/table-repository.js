fa8a76e85ee7edfa3e3686d44e46e6aa5576768a
RifqiFahrevi <fahrevifiqil@gmail.com>
2026-04-18 20:33:45 +0700

    initial commit

---
+++ src/api/components/table/table-repository.js
@@ -0,0 +1,25 @@
+const { table } = require('../../../models');
+
+async function gettable() {
+  return table.find({});
+}
+
+async function create(id, orders, date, waiter, catatan) {
+  return table.create({ id, orders, date, waiter, catatan });
+}
+async function updatetable(id, orders, date, waiter, catatan) {
+  return table.findByIdAndUpdate(
+    id,
+    { orders, date, waiter, catatan },
+    { new: true }
+  );
+}
+async function deletetable(id) {
+  return table.findByIdAndDelete(id);
+}
+module.exports = {
+  gettable,
+  create,
+  updatetable,
+  deletetable,
+};
--- src/api/routes.js
+++ src/api/routes.js
@@ -4,14 +4,17 @@ const auth = require('./components/auth/auth-route');
 const books = require('./components/books/books-route');
 const gacha = require('./components/gacha/gacha-route');
 const users = require('./components/users/users-route');
-
+const reservation = require('./components/reservation/reservation-route');
+const order = require('./components/order/order-route');
+const table = require('./components/table/table-route');
 module.exports = () => {
   const app = express.Router();
 
   auth(app);
   books(app);
   users(app);
-  gacha(app);
-
+  reservation(app);
+  order(app);
+  table(app);
   return app;
 };
